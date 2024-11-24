5246150be5dfc3ec014f1fe164e6f01751c2aef5
valeria-ab <offtotheraces@inbox.ru>
2024-11-24 14:18:38 +0300

    HW2

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -20,7 +20,7 @@ export const UserList = (props: UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-       {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+       {props.users.map((user) => (
        <li key={user.id} id={`hw01-user-${user.id}`}>
           <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
           {user.address.street}, {user.address.city}
